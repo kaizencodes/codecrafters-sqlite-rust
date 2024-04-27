811c44ee0b0967ac9d1a68b51b0a48f26a13cbf6
Daniel Szatmari <daniel.kaizencodes@gmail.com>
2024-04-27 08:04:35 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -21,20 +21,26 @@ fn main() -> Result<()> {
 
             // The page size is stored at the 16th byte offset, using 2 bytes in big-endian order
             let page_size = u16::from_be_bytes([header[16], header[17]]);
+            println!("database page size: {}", page_size);
             // make it dynamic
             let mut schema = [0; 4096 - 100];
             file.read_exact(&mut schema)?;
 
-            println!("database page size: {}", page_size);
             // println!("header: {:X?}", header);
             // println!("schema: {:X?}", schema);
 
             let table_count = u16::from_be_bytes([schema[3], schema[4]]);
-            println!("number of tables: {}", table_count - 1);
+            println!("number of tables: {}", table_count);
 
             // let offset = u16::from_be_bytes([schema[8], schema[9]]);
 
-            // println!("offset: {:X?}", &schema[(offset as usize) + 10..]);
+            // // println!("offset: {}", offset);
+            // println!("offset data: {:X?}", &schema[(offset as usize)..]);
+            // for elem in &schema[(offset as usize)..] {
+            //     println!("offset data: {:#b}", elem);
+            // }
+            // println!("offset data: {:b}", &schema[offset as usize]);
+            // println!("offset data: {:b}", &schema[(offset as usize) + 1]);
         }
         _ => bail!("Missing or invalid command passed: {}", command),
     }
